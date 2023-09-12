5bb643ba92e3f3ad63ba07e96f04022a095a51d5
joydebora <joy.debora@ui.ac.id>
2023-09-12 21:12:40 +0700

    comment

--- main/tests.py
+++ main/tests.py
@@ -1,25 +1,26 @@
-from django.test import TestCase, ItemPeliharaan
+from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
 
-class InventoriPeliharaanTest(TestCase):
+class PetkeeperInventoryTest(TestCase):
     def setUp(self):
         # Membuat data uji coba
-        ItemPeliharaan.objects.create(name="Makanan Kucing", amount=50, description="Makanan kucing premium", category="Makanan")
-        ItemPeliharaan.objects.create(name="Gembok Kandang", amount=10, description="Gembok anti-korosi", category="Perlengkapan")
+        Product.objects.create(name="Makanan Kucing", amount=50, description="Makanan kucing premium", category="Makanan")
+        Product.objects.create(name="Gembok Kandang", amount=10, description="Gembok anti-korosi", category="Perlengkapan")
 
-    def test_item_peliharaan_creation(self):
-        makanan_kucing = ItemPeliharaan.objects.get(name="Makanan Kucing")
-        gembok_kandang = ItemPeliharaan.objects.get(name="Gembok Kandang")
+    def test_item_create(self):
+        makanan_kucing = Product.objects.get(name="Makanan Kucing")
+        gembok_kandang = Product.objects.get(name="Gembok Kandang")
         
         self.assertEqual(makanan_kucing.amount, 50)
         self.assertEqual(gembok_kandang.amount, 10)
         self.assertEqual(makanan_kucing.category, "Makanan")
         self.assertEqual(gembok_kandang.category, "Perlengkapan")
 
-    def test_item_peliharaan_description(self):
-        makanan_kucing = ItemPeliharaan.objects.get(name="Makanan Kucing")
-        gembok_kandang = ItemPeliharaan.objects.get(name="Gembok Kandang")
+    def test_item_description(self):
+        makanan_kucing = Product.objects.get(name="Makanan Kucing")
+        gembok_kandang = Product.objects.get(name="Gembok Kandang")
         
         self.assertEqual(makanan_kucing.description, "Makanan kucing premium")
         self.assertEqual(gembok_kandang.description, "Gembok anti-korosi")
