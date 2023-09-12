eb694aac9c82a3d1f1ce2c4f8b4e1fe2d844f1f2
joydebora <joy.debora@ui.ac.id>
2023-09-12 21:12:16 +0700

    comment

--- main/models.py
+++ main/models.py
@@ -6,5 +6,4 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=255)
